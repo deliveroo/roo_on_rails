cbb9ae84f85ea0763f4b8fa5aa3d9a9fec6c0431
Julien Letessier <julien.letessier@gmail.com>
2017-02-08 18:36:46 +0100

    IP-460: Extract errors, helpers

---
+++ lib/roo_on_rails/checks/helpers.rb
@@ -0,0 +1,27 @@
+require 'forwardable'
+require 'singleton'
+require 'thor'
+
+module RooOnRails
+  module Checks
+    module Helpers
+      class Receiver < Thor::Group
+        include Singleton
+        include Thor::Actions
+      end
+
+      def self.included(by)
+        by.class_eval do
+          extend Forwardable
+          delegate %i[say set_color] => :'RooOnRails::Checks::Helpers::Receiver.instance'
+        end
+      end
+
+      def bold(str)
+        "\e[1;4m#{str}\e[22;24m"
+      end
+    end
+  end
+end
+
+
