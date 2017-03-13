c7a9623fcdcef56fc62631ca70ef38c67f2d2fab
Daniel Cooper <daniel@14lines.com>
2017-03-13 15:12:49 +0000

    IP-471/rubocop

--- lib/roo_on_rails/railties/http.rb
+++ lib/roo_on_rails/railties/http.rb
@@ -3,10 +3,10 @@ module RooOnRails
     class HTTP < Rails::Railtie
       initializer 'roo_on_rails.http' do |app|
         $stderr.puts 'initializer roo_on_rails.http'
-        require "rack/timeout/base"
-        require "rack/ssl-enforcer"
+        require 'rack/timeout/base'
+        require 'rack/ssl-enforcer'
 
-        require "roo_on_rails/rack/safe_timeouts"
+        require 'roo_on_rails/rack/safe_timeouts'
 
         ::Rack::Timeout.service_timeout = ENV.fetch('RACK_SERVICE_TIMEOUT', 15).to_i
         ::Rack::Timeout.wait_timeout = ENV.fetch('RACK_WAIT_TIMEOUT', 30).to_i
@@ -25,7 +25,6 @@ module RooOnRails
         )
 
         app.config.middleware.use ::Rack::Deflater
-
 
         app.config.middleware.insert_before(
           ActionDispatch::Cookies,
