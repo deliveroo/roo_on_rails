adc2380c272485138b6374f7fbcb35bbbd5e4cfe
Julien Letessier <julien.letessier@gmail.com>
2017-03-23 12:14:20 +0100

    IP-469: Uses Rspec sugar for Rails version filtering

--- spec/integration/database_spec.rb
+++ spec/integration/database_spec.rb
@@ -2,32 +2,30 @@ require 'spec_helper'
 require 'spec/support/run_test_app'
 require 'active_record'
 
-if ActiveRecord::VERSION::MAJOR >= 4
-  describe 'Database setup' do
-    run_test_app
-    let(:app_options) {{ database: 'postgresql' }}
+describe 'Database setup', rails_min_version: 4 do
+  run_test_app
+  let(:app_options) {{ database: 'postgresql' }}
 
-    before { app.wait_start }
+  before { app.wait_start }
 
-    context 'When booting' do
-      before { app_helper.shell_run "cd #{app_path} && rake db:create" }
-      after  { app_helper.shell_run "cd #{app_path} && rake db:drop" }
+  context 'When booting' do
+    before { app_helper.shell_run "cd #{app_path} && rake db:create" }
+    after  { app_helper.shell_run "cd #{app_path} && rake db:drop" }
 
-      let(:statement_timeout) { app_helper.shell_run "cd #{app_path} && rake db:statement_timeout" }
+    let(:statement_timeout) { app_helper.shell_run "cd #{app_path} && rake db:statement_timeout" }
 
-      context 'when DATABASE_STATEMENT_TIMEOUT is not set' do
-        it 'sets the statement timeout to 200ms' do
-          expect(statement_timeout).to include '200ms'
-        end
+    context 'when DATABASE_STATEMENT_TIMEOUT is not set' do
+      it 'sets the statement timeout to 200ms' do
+        expect(statement_timeout).to include '200ms'
       end
+    end
 
-      context 'when DATABASE_STATEMENT_TIMEOUT is set' do
-        before { ENV['DATABASE_STATEMENT_TIMEOUT'] = '750' }
-        after { ENV['DATABASE_STATEMENT_TIMEOUT'] = nil }
+    context 'when DATABASE_STATEMENT_TIMEOUT is set' do
+      before { ENV['DATABASE_STATEMENT_TIMEOUT'] = '750' }
+      after { ENV['DATABASE_STATEMENT_TIMEOUT'] = nil }
 
-        it 'sets the statement timeout to the value in ms' do
-          expect(statement_timeout).to include '750ms'
-        end
+      it 'sets the statement timeout to the value in ms' do
+        expect(statement_timeout).to include '750ms'
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,11 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift File.expand_path('../..', __FILE__)
 
 RSpec.configure do |config|
+  config.filter_run_excluding rails_min_version: (lambda { |_, meta|
+    require 'rails'
+    Gem::Version.new(meta[:rails_min_version]) >= Gem::Version.new(Rails::VERSION::STRING)
+  })
+
   config.around(:each, memfs: true) do |example|
     MemFs.activate { example.run }
   end
