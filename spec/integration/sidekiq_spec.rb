421df9193db74ba5197efb2168bf4703653f7d9f
Daniel Cooper <daniel@14lines.com>
2017-04-04 17:14:42 +0100

    IP-470/ensure we can start and stop sidekiq

--- spec/integration/sidekiq_spec.rb
+++ spec/integration/sidekiq_spec.rb
@@ -20,3 +20,15 @@ describe 'Sidekiq Setup' do
     end
   end
 end
+
+describe "sidekiq loader" do
+  run_sidekiq
+  before { app.wait_start }
+
+  it 'starts and stops the app cleanly' do
+    app.start.wait_start
+    app.stop
+    expect(app.status).to be_success
+  end
+
+end
