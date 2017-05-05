9b05eb29ec88b0a41878fd183e1b9c80f809dbdc
William Clarke <wmmclarke@gmail.com>
2017-05-05 09:56:32 +0100

    Allow services which do not necessarily have ActiveRecord

--- lib/roo_on_rails/railties/database.rb
+++ lib/roo_on_rails/railties/database.rb
@@ -1,7 +1,7 @@
 module RooOnRails
   module Railties
     class Database < Rails::Railtie
-      if ActiveRecord::VERSION::MAJOR >= 4
+      if defined?(ActiveRecord) && ActiveRecord::VERSION::MAJOR >= 4
         initializer 'roo_on_rails.database', after: 'active_record.initialize_database' do
           $stderr.puts 'initializer roo_on_rails.database'
 
