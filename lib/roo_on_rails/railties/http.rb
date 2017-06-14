b1025096134c868b49c7ea5102e3e33f424fe6af
Gurkan Oluc <gurkanoluc@gmail.com>
2017-06-14 15:31:50 +0100

    Make it smarter about picking middleware

--- lib/roo_on_rails/railties/http.rb
+++ lib/roo_on_rails/railties/http.rb
@@ -17,10 +17,7 @@ module RooOnRails
           ::Rack::Timeout
         )
 
-        middlewares = app.config.middleware.to_a.map { |c| c.klass.to_s }
-        middlewares.each { |p| puts p.klass.to_s }
-        
-        middleware_to_insert_before = middlewares.include?('Rack::Head') ? ::Rack::Head : ::ActionDispatch::Cookies
+        middleware_to_insert_before = Rails::VERSION::MAJOR < 4 ? ::ActionDispatch::Cookies : ::Rack::Head 
 
         # This needs to be inserted low in the stack, before Rails returns the
         # thread-current connection to the pool.
