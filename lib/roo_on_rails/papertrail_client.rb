a54ba25ecbaef3b4a6cf3ceb5fcd645784292dcb
Julien Letessier <julien.letessier@gmail.com>
2017-07-11 10:50:08 +0200

    SETI-394: Cleanup leftovers

--- lib/roo_on_rails/papertrail_client.rb
+++ lib/roo_on_rails/papertrail_client.rb
@@ -38,49 +38,6 @@ module RooOnRails
       end
     end
 
-
-    # def _get(path, q: {})
-    #   _req(path, q:q) do |uri|
-    #     Net::HTTP::Get.new(uri)
-    #   end
-    # end
-
-    # def _post(path, q: {}, body:)
-    #   _req(path, q:q) do |uri|
-    #     Net::HTTP::Post.new(uri).tap do |req|
-    #       req['Content-Type'] = 'application/json'
-    #       req.body = JSON.dump(body)
-    #     end
-    #   end
-    # end
-
-    # def _req(path, q: {})
-    #   uri = _api_url.dup
-    #   uri.path = uri.path + path
-    #   uri.query = Rack::Utils.build_query(q) if q.any?
-      
-    #   req = yield uri
-    #   req['X-Papertrail-Token'] = @token
-
-    #   response = _http.request(req)
-    #   if response['content-type'] =~ %r{^application/json}
-    #     body = JSON.parse(response.body)
-    #   else
-    #     body = response.body
-    #   end
-
-    #   case response.code.to_i
-    #   when 200..299 then # do nothing
-    #   when 400..499 then raise 
-    #   body
-    # end
-
-    # def _http
-    #   http = Net::HTTP.new(_api_url.host, _api_url.port)
-    #   http.use_ssl = true
-    #   http.start
-    # end
-
     def _api_url
       @_api_url = URI.parse('https://papertrailapp.com/api/v1')
     end
