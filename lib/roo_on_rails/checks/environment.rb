331bfc86b93af739e4af19194d0a693618795fa4
Tommaso Pavese <tommaso.pavese@deliveroo.co.uk>
2017-07-12 11:57:13 +0100

    SETI-395: load the google oauth initializer

--- lib/roo_on_rails/checks/environment.rb
+++ lib/roo_on_rails/checks/environment.rb
@@ -6,6 +6,7 @@ require 'roo_on_rails/checks/heroku/app_exists'
 require 'roo_on_rails/checks/sidekiq/settings'
 require 'roo_on_rails/checks/heroku/drains_metrics'
 require 'roo_on_rails/checks/documentation/playbook'
+require 'roo_on_rails/checks/google_oauth/initializer'
 
 module RooOnRails
   module Checks
@@ -15,6 +16,7 @@ module RooOnRails
       requires Heroku::PrebootEnabled
       requires Sidekiq::Settings
       requires Documentation::Playbook
+      requires GoogleOauth::Initializer
 
       def call
         # nothing to do
