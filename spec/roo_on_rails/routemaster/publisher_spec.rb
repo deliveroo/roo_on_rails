914c7eea15db4c30400919c355f503cf6e8d3cff
Kevin Plattret <kevin@plattret.me>
2017-07-18 15:42:19 +0200

    Add test example for #publish!

--- spec/roo_on_rails/routemaster/publisher_spec.rb
+++ spec/roo_on_rails/routemaster/publisher_spec.rb
@@ -2,40 +2,60 @@ require 'active_support/core_ext/string'
 require 'roo_on_rails/routemaster/publisher'
 
 RSpec.describe RooOnRails::Routemaster::Publisher do
-  class TestPublisher < RooOnRails::Routemaster::Publisher
+  class TestPublisherA < RooOnRails::Routemaster::Publisher
     def url
       "https://deliveroo.test/url"
     end
 
     def data
-      {
-        test_key_a: "Test value A",
-        test_key_b: "Test value B"
-      }
+      { test_key_1: "Test value 1", test_key_2: "Test value 2" }
     end
+  end
+
+  class TestPublisherB < RooOnRails::Routemaster::Publisher
   end
 
   TestModel = Class.new
   let(:model) { TestModel.new }
   let(:event) { :noop }
-  let(:publisher) { TestPublisher.new(model, event) }
 
-  it 'should be publishable' do
-    expect(publisher.publish?).to eq(true)
+  describe 'when configured correctly' do
+    let(:publisher) { TestPublisherA.new(model, event) }
+
+    it 'should publish an event to Routemaster fine' do
+      expect(::Routemaster::Client).to receive(:send).with(
+        :noop,
+        "test_models",
+        "https://deliveroo.test/url",
+        { data: {
+          "test_key_1" => "Test value 1",
+          "test_key_2" => "Test value 2"
+        }}
+      )
+      publisher.publish!
+    end
+
+    it 'should have a topic named after the model class' do
+      expect(publisher.topic).to eq("test_models")
+    end
+
+    it 'should have the correct URL' do
+      expect(publisher.url).to eq("https://deliveroo.test/url")
+    end
+
+    it 'should have the correct event type' do
+      expect(publisher.created?).to eq(false)
+      expect(publisher.updated?).to eq(false)
+      expect(publisher.deleted?).to eq(false)
+      expect(publisher.noop?).to eq(true)
+    end
   end
 
-  it 'should have a topic named after the model class' do
-    expect(publisher.topic).to eq("test_models")
-  end
+  describe 'when missing some configuration' do
+    let(:publisher) { TestPublisherB.new(model, event) }
 
-  it 'should have the correct URL' do
-    expect(publisher.url).to eq("https://deliveroo.test/url")
-  end
-
-  it 'should have the correct event type' do
-    expect(publisher.created?).to eq(false)
-    expect(publisher.updated?).to eq(false)
-    expect(publisher.deleted?).to eq(false)
-    expect(publisher.noop?).to eq(true)
+    it '#url should raise an error' do
+      expect { publisher.url }.to raise_error(NotImplementedError)
+    end
   end
 end
