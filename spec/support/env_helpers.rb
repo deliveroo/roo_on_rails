ee348c9fc43477910cde3fb25550c1202be4f9ca
Humza <humzashah@gmail.com>
2017-07-22 13:41:02 +0100

    modify spec helper

--- spec/support/env_helpers.rb
+++ spec/support/env_helpers.rb
@@ -5,6 +5,7 @@ module EnvHelpers
     allow(ENV).to receive(:[]).with(any_args).and_call_original
     allow(ENV).to receive(:fetch).with(any_args).and_call_original
 
+    allow(ENV).to receive(:has_key?).with(key).and_return(true)
     allow(ENV).to receive(:key?).with(key).and_return(true)
     allow(ENV).to receive(:[]).with(key).and_return(value)
     allow(ENV).to receive(:fetch).with(key, any_args).and_return(value)
