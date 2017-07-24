39a430fd044f91b189fd510a3ce9fead8437c6af
Julien Letessier <julien.letessier@gmail.com>
2017-07-24 16:18:24 +0200

    SETI-461: Tweak integration test setup

--- spec/support/build_test_app.rb
+++ spec/support/build_test_app.rb
@@ -5,8 +5,8 @@ require 'thor'
 module ROR
   module BuildTestApp
     ROOT = Pathname.new('../../..').expand_path(__FILE__)
-    BUNDLE_CACHE = ROOT.join('vendor/bundle')
     TEST_DIR = ROOT.join('tmp/scaffold')
+    BUNDLE_CACHE = TEST_DIR.join('bundle')
     RAILS_NEW_BASE_OPTIONS = '--skip-test --skip-git --skip-spring --skip-bundle'.freeze
 
     class Helper < Thor::Group
@@ -33,6 +33,7 @@ module ROR
 
         shell_run "rails new #{scaffold_dir} #{rails_new_options}"
 
+        require 'rails'
         if Rails::VERSION::MAJOR < 4
           append_to_file scaffold_dir.join('Gemfile'), %{
             gem 'sidekiq', '< 5'
@@ -56,7 +57,7 @@ module ROR
         #   /^\s+username:/
 
         Bundler.with_clean_env do
-          shell_run "cd #{scaffold_dir} && bundle install --path=#{BUNDLE_CACHE}"
+          shell_run "cd #{scaffold_dir} && bundle install -j4 --path=#{BUNDLE_CACHE}"
         end
 
         shell_run "tar -C #{scaffold_dir} -cf #{scaffold_path} ."
