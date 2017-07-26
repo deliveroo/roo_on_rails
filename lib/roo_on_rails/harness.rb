d0674163196b6b6d33ca9122e5d084397c20cfff
Julien Letessier <julien.letessier@gmail.com>
2017-07-26 14:51:03 +0200

    Fix check harness (#48)
    * Fix check harness
    * Actually run the check harness
    * Run environment-independent checks once
    * Make Code Climate happy
    * Fixes environment loader
    * Fixes the harness tests
    * Leftover

--- lib/roo_on_rails/harness.rb
+++ lib/roo_on_rails/harness.rb
@@ -2,6 +2,8 @@ require 'thor'
 require 'hashie'
 require 'roo_on_rails/checks/environment'
 require 'roo_on_rails/environment'
+require 'roo_on_rails/checks/sidekiq/settings'
+require 'roo_on_rails/checks/documentation/playbook'
 
 module RooOnRails
   class Harness
@@ -14,9 +16,16 @@ module RooOnRails
     end
 
     def run
-      environments.map do |env|
-        Checks::Environment.new(env: env.strip, fix: @try_fix, context: @context, dry_run: @dry_run)
-      end.each(&:run)
+      checks = [
+        Checks::Sidekiq::Settings.new(fix: @try_fix, context: @context, dry_run: @dry_run),
+        Checks::Documentation::Playbook.new(fix: @try_fix, context: @context, dry_run: @dry_run),
+      ]
+      environments.each do |env|
+        checks << Checks::Environment.new(env: env.strip, fix: @try_fix, context: @context, dry_run: @dry_run)
+      end
+
+      checks.each(&:run)
+      self
     rescue Shell::CommandFailed
       say 'A command failed to run, aborting', %i[bold red]
       exit 2
