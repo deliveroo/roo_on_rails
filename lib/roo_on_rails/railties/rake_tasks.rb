ca9c83ae7d11c37b9acbfce9ec23aa655b6ff043
Julien Letessier <julien.letessier@gmail.com>
2017-08-25 16:57:25 +0200

    fixes rake tasks loader

--- lib/roo_on_rails/railties/rake_tasks.rb
+++ lib/roo_on_rails/railties/rake_tasks.rb
@@ -2,8 +2,6 @@ module RooOnRails
   module Railties
     class RakeTasks < Rails::Railtie
       rake_tasks do
-        Rails.logger.debug 'initializer roo_on_rails.rake_tasks'
-
         Dir[File.join(__dir__, '..', 'tasks', '*.rake')].each { |f| load f }
       end
     end
