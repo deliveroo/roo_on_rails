70d250bcd7fc1966fe1e302d36f53662f3e847d6
Julien Letessier <julien.letessier@gmail.com>
2017-08-30 16:24:18 +0100

    tentative fix to app scaffolding

--- spec/support/build_test_app.rb
+++ spec/support/build_test_app.rb
@@ -65,7 +65,7 @@ module ROR
       end
 
       def unpack_scaffold_at(path)
-        path.mkpath
+				shell_run "mkdir -p #{path}"
         shell_run "tar -C #{path} -xf #{scaffold_path}"
         self
       end
