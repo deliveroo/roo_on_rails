5151af1786bb56b4b74fb3050bc19c9e3cdcd265
Ivan <abmajor7@gmail.com>
2018-02-15 11:45:15 +0000

    Set log level after instantiating logger (#89)
    * Set log level after instantiating logger
    
    * Move set_log_level to railtie
    
    * Include PR number in changelog.
    
    * Set level after initialising too
    
    As for some reason it gets lost in Rails 4+

--- lib/roo_on_rails/railties/logging.rb
+++ lib/roo_on_rails/railties/logging.rb
@@ -4,14 +4,17 @@ module RooOnRails
       initializer 'roo_on_rails.logging.before', before: :initialize_logger do
         require 'roo_on_rails/logger'
         Rails.logger = config.logger = RooOnRails::Logger.new
-        Rails.logger.debug 'initializer roo_on_rails.logging.before'
-      end
-
-      initializer 'roo_on_rails.logging.after', after: :initialize_logger do
         # It is not possible to set log_level to an invalid value without some
         # deliberate gymnastics (the setter will raise an error), and Rails
         # defaults this to `debug`, so we don't need to guard against nil /
         # invalidity
+        log_level = Rails.configuration.log_level
+
+        Rails.logger.set_log_level(default: log_level)
+        Rails.logger.debug 'initializer roo_on_rails.logging.before'
+      end
+
+      initializer 'roo_on_rails.logging.after', after: :initialize_logger do
         log_level = Rails.configuration.log_level
 
         Rails.logger.set_log_level(default: log_level)
