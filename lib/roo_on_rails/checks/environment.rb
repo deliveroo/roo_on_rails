71518557a9fb3dcae9f9c41b20ade52e1448b9fb
Tommaso Pavese <tommaso.pavese@deliveroo.co.uk>
2018-04-11 14:25:03 +0100

    Deleted references to papertrail

--- lib/roo_on_rails/checks/environment.rb
+++ lib/roo_on_rails/checks/environment.rb
@@ -3,14 +3,12 @@ require 'roo_on_rails/checks/heroku/app_exists'
 require 'roo_on_rails/checks/heroku/preboot_enabled'
 require 'roo_on_rails/checks/heroku/app_exists'
 require 'roo_on_rails/checks/heroku/drains_metrics'
-require 'roo_on_rails/checks/papertrail/all'
 
 module RooOnRails
   module Checks
     class Environment < EnvSpecific
       requires Heroku::DrainsMetrics
       requires Heroku::PrebootEnabled
-      requires Papertrail::All
 
       def call
         pass 'all good'
