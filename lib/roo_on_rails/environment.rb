7a5800b1b3bcf08fbf05179cddf9f1aa2232b0b8
Adam Buckland <adamjbuckland@gmail.com>
2019-01-24 10:23:21 +0000

    [EXPERIMENTAL] Environment Validation
    This adds the ability to specify an Envfile. This file contains a list of required environment variables, with a little logic for validating them/providing defaults. It also can ensure that certain variables are set in Hopper, rather than a dotenv file. In the event that a variable isn't set it will fail hard, and cause a Hopper rollback.

--- lib/roo_on_rails/environment.rb
+++ lib/roo_on_rails/environment.rb
@@ -1,9 +1,25 @@
 require 'dotenv'
+require 'envied'
 module RooOnRails
   class Environment
     def self.load
+      # variables from the :hopper group MUST be specified in Hopper, so cannot be loaded from dotenv file
+      if envfile_present? && hopper_environment?
+        ENVied.require(:hopper)
+      end
+
       Dotenv.load
       Dotenv.load File.expand_path('../default.env', __FILE__)
+      ENVied.require if envfile_present?
+    end
+
+    private
+    def self.envfile_present?
+      FileTest.exist?(Rails.root.join('Envfile'))
+    end
+
+    def self.hopper_environment?
+      !(Rails.env.development? || Rails.env.test?)
     end
   end
 end
--- roo_on_rails.gemspec
+++ roo_on_rails.gemspec
@@ -37,8 +37,9 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency 'faraday_middleware'
   spec.add_runtime_dependency 'routemaster-client'
   spec.add_runtime_dependency 'json-jwt', '~> 1.8'
+  spec.add_runtime_dependency 'envied', '~> 0.9'
 
-  spec.add_development_dependency 'bundler', '~> 1.13'
+  spec.add_development_dependency 'bundler', '> 1.13'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'thor', '~> 0.19'
