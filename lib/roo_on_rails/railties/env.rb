3d0a0351d0f633de9f44b673e3b480ff091f6532
Mario Gintili <mariogintili@users.noreply.github.com>
2020-05-26 13:14:25 +0100

    [DEFF-565] replace calls to logger.with with just logger (#119)
    * DEFF-565 replace calls to logger.with with just logger
    
    * rubocop fixes
    
    * more rubocop fixes
    
    * DEFF-565 rubocop fixes
    
    * rubocop
    
    * set auth method to trust
    
    * DEFF-565 skip bootsnap -> https://github.com/Shopify/bootsnap/issues/73
    
    * set base config for db
    
    * set password to nothing and dbname to the one matching database.yml file
    
    * DEFF-565 snap cache
    
    * DEFF-565 set auth method
    
    set uset to postgres
    
    propagate config to other containers, this is the step prior to a refactor
    
    ignore support folder
    
    * DEFF-565 cleanup
    
    * DEFF-565 refactor

--- lib/roo_on_rails/railties/env.rb
+++ lib/roo_on_rails/railties/env.rb
@@ -1,11 +1,9 @@
 module RooOnRails
   class Railtie < Rails::Railtie
     initializer 'roo_on_rails.default_env' do
-      Rails.logger.with initializer: 'roo_on_rails.default_env' do |log|
-        log.debug 'loading'
-        require 'roo_on_rails/environment'
-        RooOnRails::Environment.load
-      end
+      Rails.logger.debug '[roo_on_rails.default_env] loading'
+      require 'roo_on_rails/environment'
+      RooOnRails::Environment.load
     end
   end
 end
