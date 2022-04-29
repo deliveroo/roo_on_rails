e7c6c9efee888480fc01c23d1bf351e9ec5dd3ce
Lewis Monteith <lewismonteith@gmail.com>
2022-04-29 11:22:58 +0100

    Fix frozen config issue in 6.1
    Make the methods static as it's not instantiated
    
    Class does not exist
    
    Use one giant if else to check if it works
    
    Add some debug
    
    Debug
    
    Debug
    
    Chang another place that modifies the config
    
    Honor Rails and don't try and modify things
    
    Fix error
    
    Fix command for rails 6.1
    
    Alter tests for rails versions
    
    Try a different tactic
    
    Debug
    
    Try and use the old style
    
    Replace other instance of config

--- lib/roo_on_rails/railties/database.rb
+++ lib/roo_on_rails/railties/database.rb
@@ -5,17 +5,21 @@ module RooOnRails
         ActiveSupport.on_load :active_record do
           Rails.logger.debug('[roo_on_rails.database] loading')
 
-          config = ActiveRecord::Base.configurations[Rails.env]
+          config = ActiveRecord::Base.configurations[Rails.env].dup
+
           config['variables'] ||= {}
           statement_timeout = ENV.fetch('DATABASE_STATEMENT_TIMEOUT', 200)
+
           # Use -1 to disable setting the statement timeout
           unless statement_timeout == '-1'
             config['variables']['statement_timeout'] = statement_timeout
           end
+
           if ENV.key?('DATABASE_REAPING_FREQUENCY')
             config['reaping_frequency'] = ENV['DATABASE_REAPING_FREQUENCY']
           end
-          ActiveRecord::Base.establish_connection
+
+          ActiveRecord::Base.establish_connection(config)
         end
       end
     end
--- lib/roo_on_rails/tasks/db.rake
+++ lib/roo_on_rails/tasks/db.rake
@@ -8,12 +8,10 @@ if defined?(ActiveRecord)
 
     namespace :migrate do
       task extend_statement_timeout: :environment do
-        if ActiveRecord::VERSION::MAJOR >= 4
-          config = ActiveRecord::Base.configurations[Rails.env]
-          config['variables'] ||= {}
-          config['variables']['statement_timeout'] = ENV.fetch('MIGRATION_STATEMENT_TIMEOUT', 10_000)
-          ActiveRecord::Base.establish_connection
-        end
+        config = ActiveRecord::Base.configurations[Rails.env].dup
+        config['variables'] ||= {}
+        config['variables']['statement_timeout'] = ENV.fetch('MIGRATION_STATEMENT_TIMEOUT', 10_000)
+        ActiveRecord::Base.establish_connection(config)
       end
     end
   end
