a537ffb34b10f861814abbb70fc282ff9bc44786
Lewis Monteith <lewismonteith@gmail.com>
2022-05-12 10:23:04 +0100

    Don't return

--- lib/roo_on_rails/railties/database.rb
+++ lib/roo_on_rails/railties/database.rb
@@ -3,20 +3,21 @@ module RooOnRails
     class Database < Rails::Railtie
       initializer 'roo_on_rails.database', after: 'active_record.initialize_database' do
         ActiveSupport.on_load :active_record do
-          return if Rails::VERSION::MAJOR >= 7
-
           Rails.logger.debug('[roo_on_rails.database] loading')
 
-          config = ActiveRecord::Base.configurations[Rails.env]
-          config['variables'] ||= {}
-          statement_timeout = ENV.fetch('DATABASE_STATEMENT_TIMEOUT', 200)
-          # Use -1 to disable setting the statement timeout
-          unless statement_timeout == '-1'
-            config['variables']['statement_timeout'] = statement_timeout
+          if Rails::VERSION::MAJOR < 7
+            config = ActiveRecord::Base.configurations[Rails.env]
+            config['variables'] ||= {}
+            statement_timeout = ENV.fetch('DATABASE_STATEMENT_TIMEOUT', 200)
+            # Use -1 to disable setting the statement timeout
+            unless statement_timeout == '-1'
+              config['variables']['statement_timeout'] = statement_timeout
+            end
+            if ENV.key?('DATABASE_REAPING_FREQUENCY')
+              config['reaping_frequency'] = ENV['DATABASE_REAPING_FREQUENCY']
+            end
           end
-          if ENV.key?('DATABASE_REAPING_FREQUENCY')
-            config['reaping_frequency'] = ENV['DATABASE_REAPING_FREQUENCY']
-          end
+
           ActiveRecord::Base.establish_connection
         end
       end
