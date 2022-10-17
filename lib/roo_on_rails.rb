428f9a3a1f28fe51d3200daf03dddf3b99c53273
Alex Smn <alexandrusmn@gmail.com>
2022-10-17 14:38:43 +0100

    IM-1122: removes DB default setup

--- lib/roo_on_rails.rb
+++ lib/roo_on_rails.rb
@@ -6,7 +6,6 @@ end
 if defined?(Rails)
   require 'dotenv/rails-now'
   require 'roo_on_rails/railties/env'
-  require 'roo_on_rails/railties/database'
   require 'roo_on_rails/railties/http'
   require 'roo_on_rails/railties/sidekiq_integration'
   require 'roo_on_rails/railties/rake_tasks'
